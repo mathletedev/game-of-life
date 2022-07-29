479e36db938e996c02ccc9057472870c09dafd23
Neo Delta <mathletedev@gmail.com>
2022-07-28 23:50:54 -0700

    PROJECT COMPLETE!!!

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use ggez::{
 	conf::WindowMode,
 	event::{self, EventHandler, MouseButton},
-	graphics::{self, Color, DrawMode, Mesh, Rect},
+	graphics::{self, Color, DrawMode, Mesh, Rect, Text},
 	mint::Point2,
-	Context, ContextBuilder, GameError, GameResult,
+	timer, Context, ContextBuilder, GameError, GameResult,
 };
 
 const CELL_SIZE: (f32, f32) = (20.0, 20.0);
-const GRID_SIZE: (f32, f32) = (30.0, 20.0);
+const GRID_SIZE: (f32, f32) = (40.0, 40.0);
 const WINDOW_SIZE: (f32, f32) = (CELL_SIZE.0 * GRID_SIZE.0, CELL_SIZE.1 * GRID_SIZE.1);
-const LINE_WIDTH: f32 = 1.0;
+const BG_COLOR: Color = Color::WHITE;
+const CELL_COLOR: Color = Color::BLACK;
+const LINE_WIDTH: f32 = 2.0;
+const LINE_COLOR: Color = Color {
+	r: 0.5,
+	g: 0.5,
+	b: 0.5,
+	a: 1.0,
+};
+const TEXT_COLOR: Color = Color::BLACK;
 
 struct State {
 	grid: Vec<Vec<bool>>,
+	fps: u32,
 	running: bool,
 }
 
@@ -20,18 +30,65 @@ impl State {
 	pub fn new() -> Self {
 		State {
 			grid: vec![vec![false; GRID_SIZE.1 as usize]; GRID_SIZE.0 as usize],
+			fps: 1,
 			running: false,
 		}
 	}
 }
 
 impl EventHandler<GameError> for State {
-	fn update(&mut self, _ctx: &mut Context) -> GameResult {
+	fn update(&mut self, ctx: &mut Context) -> GameResult {
+		while timer::check_update_time(ctx, self.fps) && self.running {
+			let mut coords: Vec<(usize, usize)> = vec![];
+
+			for i in 0..GRID_SIZE.0 as usize {
+				let left = if i > 0 {
+					i - 1
+				} else {
+					GRID_SIZE.0 as usize - 1
+				};
+				let right = if i < GRID_SIZE.0 as usize - 1 {
+					i + 1
+				} else {
+					0
+				};
+				for j in 0..GRID_SIZE.1 as usize {
+					let up = if j > 0 {
+						j - 1
+					} else {
+						GRID_SIZE.1 as usize - 1
+					};
+					let down = if j < GRID_SIZE.1 as usize - 1 {
+						j + 1
+					} else {
+						0
+					};
+
+					let neighbors =
+						self.grid[left][j] as u8
+							+ self.grid[left][up] as u8 + self.grid[i][up] as u8
+							+ self.grid[right][up] as u8 + self.grid[right][j] as u8
+							+ self.grid[right][down] as u8
+							+ self.grid[i][down] as u8 + self.grid[left][down] as u8;
+
+					if self.grid[i][j] && (neighbors < 2 || neighbors > 3) {
+						coords.push((i, j));
+					} else if !self.grid[i][j] && neighbors == 3 {
+						coords.push((i, j));
+					}
+				}
+			}
+
+			for coord in coords {
+				self.grid[coord.0][coord.1] ^= true;
+			}
+		}
+
 		Ok(())
 	}
 
 	fn draw(&mut self, ctx: &mut Context) -> GameResult {
-		graphics::clear(ctx, Color::BLACK);
+		graphics::clear(ctx, BG_COLOR);
 
 		for i in 0..GRID_SIZE.0 as usize {
 			for j in 0..GRID_SIZE.1 as usize {
@@ -45,7 +102,7 @@ impl EventHandler<GameError> for State {
 							CELL_SIZE.0,
 							CELL_SIZE.1,
 						),
-						Color::WHITE,
+						CELL_COLOR,
 					)?;
 					graphics::draw(ctx, &rect, (Point2 { x: 0.0, y: 0.0 },))?;
 				}
@@ -67,7 +124,7 @@ impl EventHandler<GameError> for State {
 						},
 					],
 					LINE_WIDTH,
-					Color::WHITE,
+					LINE_COLOR,
 				)?;
 				graphics::draw(ctx, &line, (Point2 { x: 0.0, y: 0.0 },))?;
 			}
@@ -89,10 +146,13 @@ impl EventHandler<GameError> for State {
 					},
 				],
 				LINE_WIDTH,
-				Color::WHITE,
+				LINE_COLOR,
 			)?;
 			graphics::draw(ctx, &line, (Point2 { x: 0.0, y: 0.0 },))?;
 		}
+
+		let text = Text::new(self.fps.to_string());
+		graphics::draw(ctx, &text, (Point2 { x: 0.0, y: 2.0 }, TEXT_COLOR))?;
 
 		graphics::present(ctx)?;
 
@@ -106,8 +166,7 @@ impl EventHandler<GameError> for State {
 		x: f32,
 		y: f32,
 	) {
-		self.grid[(x / CELL_SIZE.0).floor() as usize][(y / CELL_SIZE.1).floor() as usize] =
-			!self.grid[(x / CELL_SIZE.0).floor() as usize][(y / CELL_SIZE.1).floor() as usize];
+		self.grid[(x / CELL_SIZE.0).floor() as usize][(y / CELL_SIZE.1).floor() as usize] ^= true;
 	}
 
 	fn key_down_event(
@@ -118,7 +177,16 @@ impl EventHandler<GameError> for State {
 		repeat: bool,
 	) {
 		if keycode == event::KeyCode::Space && !repeat {
-			self.running = !self.running;
+			self.running ^= true;
+		}
+		if keycode == event::KeyCode::Up {
+			self.fps += 1;
+		}
+		if keycode == event::KeyCode::Down && self.fps > 1 {
+			self.fps -= 1;
+		}
+		if keycode == event::KeyCode::Delete {
+			self.grid = vec![vec![false; GRID_SIZE.1 as usize]; GRID_SIZE.0 as usize];
 		}
 	}
 }
